a081710adbbeef59e4f21207c6d36940860fe509
SanaIshaqat <>
2021-06-30 13:13:50 +0000

    Java Scripted

---
+++ app.js
@@ -0,0 +1,19 @@
+alert ("Welcome to my Website!")
+
+
+var name = prompt ("What is your name?")
+alert ( "Welcome to my website " + name + " :) " )
+
+console.log (name)
+
+var favoritearchitecturalstyle = prompt ("What Is Your Favorite Architectural Style?")
+
+console.log (favoritearchitecturalstyle)
+
+
+
+if (favoritearchitecturalstyle == "Modern")
+{alert ("You Entered Modern")}
+else if (favoritearchitecturalstyle != null)
+{document.write ("<h4>" + favoritearchitecturalstyle + "</h4>")}
+else {alert ("You didn't enter anything")}
